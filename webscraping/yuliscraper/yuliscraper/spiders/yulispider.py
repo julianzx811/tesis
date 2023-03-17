7f4bbf09b066d9e256a9087794ecac4561aa757b
yulian david <julianolomanolox@hotmail.com>
2023-03-17 13:05:33 -0500

    part 1 finished, getting data and cleaning a bit, also moving to other page

--- webscraping/yuliscraper/yuliscraper/spiders/yulispider.py
+++ webscraping/yuliscraper/yuliscraper/spiders/yulispider.py
@@ -3,8 +3,23 @@ import scrapy
 
 class YulispiderSpider(scrapy.Spider):
     name = "yulispider"
-    allowed_domains = ["yuli.co.uk"]
     start_urls = ["https://www.chocolate.co.uk/collections/all"]
 
     def parse(self, response):
-        pass
+        products = response.css("product-item")
+        for product in products:
+            yield {
+                "name": product.css("a.product-item-meta__title::text").get(),
+                "url": products.css("div.product-item-meta a").attrib["href"],
+                "price": product.css("span.price")
+                .get()
+                .replace(
+                    '<span class="price">\n              <span class="visually-hidden">Sale price</span>',
+                    "",
+                )
+                .replace("</span>", ""),
+            }
+        next_page = response.css('[rel="next"]::attr(href)').get()
+        if next_page is not True:
+            next_page_url = "https://www.chocolate.co.uk" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
