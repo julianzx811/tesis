a570e514f3da62a54c5fbe80120ef64ba71d3119
yulian david <julianolomanolox@hotmail.com>
2023-03-29 16:57:23 -0500

    bluetooth and some cleaning configuration

--- webscraping/yuliscraper/yuliscraper/spiders/yulispider.py
+++ webscraping/yuliscraper/yuliscraper/spiders/yulispider.py
@@ -10,14 +10,11 @@ class YulispiderSpider(scrapy.Spider):
         products = response.css("product-item")
         productItem = Product()
         for product in products:
-            productItem["name"]: product.css("a.product-item-meta__title::text").get()
-            productItem["url"]: products.css("div.product-item-meta a").attrib["href"]
-            productItem["price"]: product.css("span.price").get().replace(
-                '<span class="price">\n              <span class="visually-hidden">Sale price</span>',
-                "",
-            ).replace("</span>", "")
-            yield productItem
-        next_page = response.css('[rel="next"]::attr(href)').get()
+             chocolate = ChocolateProductLoader(item=ChocolateProduct(), selector=product)
+            chocolate.add_css('name', "a.product-item-meta__title::text")
+            chocolate.add_css('price', 'span.price', re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>')
+            chocolate.add_css('url', 'div.product-item-meta a::attr(href)')
+            yield chocolate.load_item()       next_page = response.css('[rel="next"]::attr(href)').get()
         if next_page is not True:
             next_page_url = "https://www.chocolate.co.uk" + next_page
             yield response.follow(next_page_url, callback=self.parse)
