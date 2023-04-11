11fa4f7b6d96e08e0c2f3558edf962d770736343
yulian david <julianolomanolox@hotmail.com>
2023-04-11 17:04:57 -0500

    chapter 2 finished

--- webscraping/yuliscraper/yuliscraper/items.py
+++ webscraping/yuliscraper/yuliscraper/items.py
@@ -6,9 +6,7 @@
 import scrapy
 
 
-class Product(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class yuliProduct(scrapy.Item):
     name = scrapy.Field()
     price = scrapy.Field()
     url = scrapy.Field()
--- webscraping/yuliscraper/yuliscraper/spiders/yulispider.py
+++ webscraping/yuliscraper/yuliscraper/spiders/yulispider.py
@@ -1,5 +1,6 @@
 import scrapy
-from yuliscraper.items import Product
+from yuliscraper.items import yuliProduct
+from yuliscraper.itemsloaders import yuliProductLoader
 
 
 class YulispiderSpider(scrapy.Spider):
@@ -8,13 +9,18 @@ class YulispiderSpider(scrapy.Spider):
 
     def parse(self, response):
         products = response.css("product-item")
-        productItem = Product()
+        productItem = yuliProduct()
         for product in products:
-             chocolate = ChocolateProductLoader(item=ChocolateProduct(), selector=product)
-            chocolate.add_css('name', "a.product-item-meta__title::text")
-            chocolate.add_css('price', 'span.price', re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>')
-            chocolate.add_css('url', 'div.product-item-meta a::attr(href)')
-            yield chocolate.load_item()       next_page = response.css('[rel="next"]::attr(href)').get()
+            chocolate = yuliProductLoader(item=yuliProduct(), selector=product)
+            chocolate.add_css("name", "a.product-item-meta__title::text")
+            chocolate.add_css(
+                "price",
+                "span.price",
+                re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>',
+            )
+            chocolate.add_css("url", "div.product-item-meta a::attr(href)")
+            yield chocolate.load_item()
+            next_page = response.css('[rel="next"]::attr(href)').get()
         if next_page is not True:
             next_page_url = "https://www.chocolate.co.uk" + next_page
             yield response.follow(next_page_url, callback=self.parse)
