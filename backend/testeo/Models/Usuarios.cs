4b6df83079153b44fb66ab2a258c03a2c33a93b9
yulian david <julianolomanolox@hotmail.com>
2023-06-24 00:01:20 -0500

    testing db and basic api in azure

---
+++ backend/testeo/Models/Usuarios.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testeo.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public required int Usuario_id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+    }
+}
