e8c09c2ac21a066da9c54af20ebd856992835660
yulian david <julianolomanolox@hotmail.com>
2023-08-04 11:32:15 -0500

    products api

---
+++ backend/ProductsApi/src/index.js
@@ -0,0 +1,18 @@
+// In src/index.js
+const express = require("express");
+
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// For testing purposes
+app.get("/", (req, res) => {
+  res.send("<h2>It's Working!</h2>");
+});
+
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
---
+++ backend/ProductsApi/src/services/workoutService.js
@@ -0,0 +1,30 @@
+const Workout = require("../database/Workout");
+
+const getAllWorkouts = () => {
+  const workouts = Workout.getAllWorkouts();
+  return workouts;
+};
+
+const getOneWorkout = () => {
+  return;
+};
+
+const createNewWorkout = () => {
+  return;
+};
+
+const updateOneWorkout = () => {
+  return;
+};
+
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
