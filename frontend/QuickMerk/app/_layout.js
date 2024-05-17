052eb638c41f30a178e730a4f5bbb9b69cd029e4
yulian david <julianolomanolox@hotmail.com>
2024-05-17 16:55:44 -0500

    products api working with word2vec seding images

--- frontend/QuickMerk/app/_layout.js
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-const StackLayout = () => {
-  return (
-    <Stack>
-      <Stack.Screen name="(menu)" options={{ headerTintColor: "#99094a" }} />
-    </Stack>
-  );
-};
--- frontend/QuickMerk/app/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import UserReducer from "./reducers/UserReducer";
-
-const rootReducer = combineReducers({
-  user: UserReducer,
-});
-
-export default store = createStore(rootReducer);
